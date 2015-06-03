46a5afddd2c4a67e4ec534169213b75587481282
Chris Baker <bakerc611@gmail.com>
2015-06-02 20:24:57 -0700

    message received, over and out

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
  alert("I'm working with JavaScript");
+function mad_libs() {
+	document.write('Long ago, a group of penguins were lost in a land far far away. Trying diligently to find their way back home, they decided they would find the nearest body of water. Soon, they ran into Kong, a wise ape with a fondness for nature. "Be wary penguins" he said as the waddled toward the river, "The mighty bear is hungry and searching for food". Still, they pressed on, determined to find home. Shortly thereafter, they found a beautiful river. Unfortunately, due to poor eyesight and their small stature, they thought they had finally reached the ocean that would take them home. They had instead, entered the hunting grounds of the mighty bear. The End.');
+}
+mad_libs();
